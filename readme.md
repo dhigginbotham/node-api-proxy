8f01b8e6fd1d6cdca44344eb63574cef5121fed2
David Higginbotham <davehigginbotham@gmail.com>
2013-10-15 21:46:42 -0600

    ignore these things, seriously

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 ## node-api-proxy (napip)
-along came a spider, this spider needed to proxy api's to test them, build out other things that relied heavily on pointless roundtrips. in comes `request` and `nedb/mongodb` to the rescue. once i found these gems i wrote `localize-api` in `CoffeeScript` later i decided i'd like to go back to javascript, so here we are -- same api, just manually refactored to plain ole javascript.
+along came a spider, this spider needed to proxy api's to test them, build out other things that relied heavily on pointless roundtrips. in comes `request` and `nedb/mongodb` to the rescue. once i found these gems i wrote `localize-api` in `CoffeeScript` later i decided i'd like to go back to `JavaScript`, so here we are -- same api, just manually refactored to plain ole `JavaScript`.
 
 <3
 
---
+++ test/helpers.js
@@ -0,0 +1,102 @@
+var _ = require('lodash');
+var expect = require('expect.js');
+
+var helpers = require('../../lib/helpers');
+
+describe('test helpers.js methods to ensure they\'re working the way we rely on them to', function () {
+
+  var hasTrailingSlashUri = 'https://api.github.com/';
+  var noTrailingSlashUri = 'https://api.github.com';
+
+  var trailingSlashPath = '/path/of/endpoint/n';
+  var noTrailingSlashPath = '/path/of/endpoint/n/';
+  
+  var trix = '/*/';
+  var sanitizedTrix = '/*';
+
+  describe('non-blocking: test removeTrailingSlash', function () {
+
+    helpers.removeTrailingSlash(hasTrailingSlashUri, function (helperResult) {
+
+      it('goes from ' + hasTrailingSlashUri + ' to ' + noTrailingSlashUri, function (done) {
+
+        expect(helperResult).not.to.be(null);
+
+        expect(helperResult).to.equal(noTrailingSlashUri);
+        
+        return done();
+
+      });
+
+    });
+
+    helpers.removeTrailingSlash(noTrailingSlashPath, function (helperResult) {
+
+      it('goes from ' + noTrailingSlashPath + ' to ' + trailingSlashPath, function (done) {
+
+        expect(helperResult).not.to.be(null);
+
+        expect(helperResult).to.equal(trailingSlashPath);
+        
+        return done();
+
+      });
+
+    });
+
+    helpers.removeTrailingSlash(trix, function (helperResult) {
+
+      it('goes from ' + trix + ' to ' + sanitizedTrix, function (done) {
+
+        expect(helperResult).not.to.be(null);
+
+        expect(helperResult).to.equal(sanitizedTrix);
+        
+        return done();
+
+      });
+
+    });
+
+  });
+
+  describe('blocking: test removeTrailingSlash', function () {
+
+    it('goes from ' + hasTrailingSlashUri + ' to ' + noTrailingSlashUri, function (done) {
+      
+      var helperResult = helpers.removeTrailingSlash(hasTrailingSlashUri);
+
+      expect(helperResult).not.to.be(null);
+      expect(helperResult).to.equal(noTrailingSlashUri);
+
+      return done();
+
+    });
+
+    it('goes from ' + noTrailingSlashPath + ' to ' + trailingSlashPath, function (done) {
+      
+      var helperResult = helpers.removeTrailingSlash(noTrailingSlashPath);
+
+      expect(helperResult).not.to.be(null);
+      expect(helperResult).to.equal(trailingSlashPath);
+
+      return done();
+
+    });
+  
+    it('goes from ' + trix + ' to ' + sanitizedTrix, function (done) {
+      
+      var helperResult = helpers.removeTrailingSlash(trix);
+
+      expect(helperResult).not.to.be(null);
+      expect(helperResult).to.equal(sanitizedTrix);
+
+      return done();
+
+    });
+  
+  });
+
+  // end of removeTrailingSlash tests
+
+});
